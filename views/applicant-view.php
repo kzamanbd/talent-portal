2e7fdbe6ae285a9b920443056d282923581b3e28
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-09-04 00:02:51 +0600

    update pot file and locale

--- views/applicant-view.php
+++ views/applicant-view.php
@@ -3,7 +3,7 @@
 
     <?php if ( isset( $_GET[ 'talent-deleted' ] ) && $_GET[ 'talent-deleted' ] == 'true' ) {?>
         <div class="notice notice-success">
-            <p><?php _e( 'Talent has been deleted successfully!', 'talent-portal' );?></p>
+            <p><?php _e( 'Application has been deleted successfully!', 'talent-portal' );?></p>
         </div>
     <?php }?>
 
